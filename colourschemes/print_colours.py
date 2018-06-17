2a8ef58fd919d598047b59c75d01f60faf6521e8
James Andrew <james.andrew@metaswitch.com>
2018-06-17 11:04:08 +0000

    Add basic colourscheme bits

--- colourschemes/print_colours.py
+++ colourschemes/print_colours.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import argparse
 import json
 
@@ -10,6 +12,8 @@ def parse_args():
 
 
 def hex_to_rgb(hex_colour):
+    if isinstance(hex_colour, str):
+        hex_colour = int(hex_colour, 16)
     return tuple((hex_colour >> shift) & 0xFF for shift in (16, 8, 0))
 
 
@@ -18,6 +22,10 @@ def main():
     hexmap = {}
     with open(args.inputfile, "r") as fd:
         hexmap = json.load(fd)
+
+    for colour, subcolours in hexmap.items():
+        for sc_name, sc_hex in subcolours.items():
+            print("Colour: {} {} {}".format(colour, sc_name, hex_to_rgb(sc_hex)))
 
 
 if __name__ == "__main__":
