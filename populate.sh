7834d950706569de4c6fc119e6eb29b3ded3d136
James Andrew <james.andrew@metaswitch.com>
2018-07-19 10:35:54 +0100

    add ctags config

--- populate.sh
+++ populate.sh
@@ -4,4 +4,5 @@ echo 'source $HOME/dotfiles/bash/bashrc_main.sh' >> ~/.bashrc
 cp ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
 cp ~/dotfiles/vimrc ~/.vimrc
 cp ~/dotfiles/gitconfig ~/.gitconfig
+cp ~/dotfiles/ctags ~/.ctags
 # cp -r ~/dotfiles/scripts ~/
